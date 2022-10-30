25d4884ac7ce0c3918b821a933aad21ff62509e9
Pranith P Alva <alvapranith@gmail.com>
2022-10-30 15:36:25 +0530

    Completed form validation for password
    	Now the error message pop ups if passwords don't
    	match.

--- main.js
+++ main.js
@@ -1,5 +1,42 @@
 const password = document.getElementById("password");
 const confPassword = document.getElementById("confirm-password");
+const errorMessage = document.querySelector("#password-missmatch");
+const dumy = document.querySelector("#dumy");
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone-number");
+const countryCode = document.getElementById("country-code");
+
+function operation(e) {
+	errorMessage.classList.add("hidden");
+	errorMessage.classList.remove("visible");
+	dumy.classList.add("hidden");
+	dumy.classList.remove("visible");
+	if (
+		password.value != null &&
+		confPassword.value != null &&
+		firstName != null &&
+		lastName != null &&
+		email != null &&
+		phone != null &&
+		countryCode != null
+	) {
+		if (password.value != confPassword.value) {
+			console.log(e);
+			e.preventDefault();
+			password.style = "border-color:red";
+			confPassword.style = "border-color:red";
+			errorMessage.classList.remove("hidden");
+			errorMessage.classList.add("visible");
+			dumy.classList.remove("hidden");
+			dumy.classList.add("visible");
+		}
+	}
+}
 
 console.log(password);
 console.log(confPassword);
+
+const button = document.querySelector("button");
+button.addEventListener("click", operation);
